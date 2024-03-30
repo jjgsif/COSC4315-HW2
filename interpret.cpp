0b113edc4a5dc117d5f576ceeafb2c872e82eec7
Leafsicle77 <chazxavi88@gmail.com>
2024-03-29 22:09:49 -0500

    Page 311 start progress save atm

--- interpret.cpp
+++ interpret.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdlib.h>
 #include "scanner.cpp"
 
 
@@ -12,12 +13,49 @@ using namespace std;
 struct Parser{
     Token current;
     Token previous;
+    bool hadError;
+    bool panicMode;
 };
 
+typedef enum{
+    PREC_NONE,
+    PREC_ASSIGNMENT,    // =
+    PREC_OR,            // or
+    PREC_AND,           // and
+    PREC_EQUALITY,      // == !=
+    PREC_COMPARISON,    // < > <= >=
+    PREC_TERM,          // + -
+    PREC_FACTOR,        // * /
+    PREC_UNARY,         // ! -
+    PREC_CALL,          // . ()
+    PREC_PRIMARY
+}Precedence;
 
 Parser parser;
 
+Chunk* compilingChunk;
 
+static Chunk* currentChunk(){
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message){
+    if(parser.panicMode) return;
+    parser.panicMode = true;
+
+    
+    cout<<"Error at line: "<<token->line;
+    if(token->type == TokenType::TOKEN_EOF){
+        cout<<" at end";
+    }
+    else if(token->type == TokenType::TOKEN_ERROR){
+    }
+    else{
+        cout<<" at "<<token->length<<token->start;
+    }
+    cout<<message<<endl;
+    parser.hadError = true;
+}
 
 static void advance() {
     parser.previous = parser.current;
@@ -30,14 +68,97 @@ static void advance() {
     }
 }
 
+static void consume(TokenType type, const char* message){
+    if (parser.current.type == type){
+        advance();
+        return;
+    }
+    errorAtCurrent(message);
+}
 
+static void emitByte(uint8_t byte){
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2){
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn(){
+    emitByte(OP_RETURN);
+}
+
+static void emitConstant(Value value){
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void number(bool canAssign){
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary(bool canAssign){
+    TokenType operatorType = parser.previous.type;
+
+    parsePrecedence(PREC_UNARY);
+
+    switch(operatorType){
+        case TokenType::TOKEN_MINUS : emitByte(OP_NEGATE); break;
+        default:
+            return; 
+    }
+
+}
+
+static void parsePrecedence(Precedence precedence){
+
+}
+
+static uint8_t makeConstant(Value value){
+    int constant = addConstant(currentChunk(), value);
+    if(constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void endCompiler(){
+    emitReturn();
+}
+
+static void expression(){
+    parsePrecedence(PREC_ASSIGNMENT)
+}
+
+static void grouping(bool canAssign){
+    expression();
+    consume(TokenType::TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
 
 bool compile(char* sourceCode, Chunk* chunk){
     initScanner(sourceCode);
+    compilingChunk = chunk;
     
+    parser.hadError = false;
+    parser.panicMode = false;
     advance();
     expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+    consume(TokenType::TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
+
+
 #pragma GCC diagnostic pop
+
+static void errorAtCurrent(const char* message){
+    errorAt(&parser.current, message);
+}
+
+static void error(const char* message){
+    errorAt(&parser.previous, message);
+}
 
