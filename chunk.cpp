09b39fd65210b415fd9affc407da5d1530b57d5e
JJGSIF <jjgsif@gmail.com>
2024-03-29 19:07:56 -0500

    Chunk

--- chunk.cpp
+++ chunk.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 enum OpCode {
     OP_RETURN,
+    OP_CONSTANT
 };
 
 typedef double Value;
@@ -21,12 +22,24 @@ struct Chunk{
     int count;
     int capacity;
     u_int8_t* code;
+    int* lines;
+    ValueArray constants;
 };
 
 void initChunk(Chunk* chunk){
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = nullptr;
+    chunk->lines = nullptr;
+    initValueArray(&chunk->constants);
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset){
+    u_int8_t constant = chunk->code[offset+1];
+    printf("%-16s %d '", name, constant);
+    printf("%g",chunk->constants.values[constant]);
+    printf("'\n'");
+    return offset+2;
 }
 
 
@@ -50,19 +63,20 @@ void initChunk(Chunk* chunk){
     return result;
  }
 
-void writeChunk(Chunk* chunk, u_int8_t byte){
+void writeChunk(Chunk* chunk, u_int8_t byte, int line){
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
- chunk->capacity = GROW_CAPACITY(oldCapacity);
- chunk->code = GROW_ARRAY(u_int8_t, chunk->code,
- oldCapacity, chunk->capacity);
- }
- chunk->code[chunk->count] = byte;
- chunk->count++;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(u_int8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
+    }
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
 }
 
 void freeChunk(Chunk* chunk){
     FREE_ARRAY(u_int8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     initChunk(chunk);
 }
 
@@ -76,6 +90,8 @@ int disassembleInstruction(Chunk* chunk, int offset){
     printf("%04d ", offset);
     u_int8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT:
+        return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
         default:
@@ -98,11 +114,24 @@ void initValueArray(ValueArray* array){
 }
 
 void writeValueArray(ValueArray* array, Value value){
+    if(array->capacity < array->count + 1){
+        int oldCapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldCapacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+    }
 
+    array->values[array->count] = value;
+    array->count++;
 }
 
 void freeValueArray(ValueArray* array){
+    FREE_ARRAY(Value, array->values, array->capacity);
+    initValueArray(array);
+}
 
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
 }
 
 
